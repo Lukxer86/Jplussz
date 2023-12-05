13d383782c07b2184317810a1ce383866301fcc5
gere_lukacs <gere_lukacs@SZT2-06>
2023-12-05 10:49:00 +0100

    first commit

---
+++ j.sql
@@ -0,0 +1,18 @@
+SELECT Count(*)
+FROM Jplussz;
+
+SELECT Count(*)
+FROM Jplussz
+Where (nem = 0);
+
+SELECT nev
+FROM Jplussz
+Where (testverek = 0);
+
+SELECT Count(*)
+FROM Jplussz
+Where (testverek = 0);
+
+SELECT nev
+FROM Jplussz
+Where (angol like "2*");
